e104d29567e2a0cdededdd5020c2dc5d76de43b4
Dima <test>
2021-12-18 21:57:16 +0300

    6 тест-кейсов готовы

--- auto.py
+++ auto.py
@@ -7,43 +7,46 @@ driver = webdriver.Chrome()
 
 driver.get('https://www.asos.com/ru/men/')
 
+
 # TК-005	Проверка функции поиск по сайту
 search_box = driver.find_element(By.XPATH,
                                  '//*[@id="chrome-search"]')
-search_box.send_keys('Рубашка')
+search_box.send_keys('Галстук')
 time.sleep(1)
 search_button = driver.find_element(By.XPATH,
                                     '//*[@id="chrome-sticky-header"]/div[1]/div/div/div/form/div/button')
 search_button.click()
 
-answer = driver.find_element(By.XPATH, '//*[@id="search-term-banner"]/p[2]')
+answer = driver.find_element(By.XPATH,
+                             '//*[@id="search-term-banner"]/p[2]')
 
-if answer.text != u'рубашка':
-    print("Ожидаемый ответ верен =  %r" % answer.text)
+if answer.text != u'галстук':
+    print("№1 Ожидаемый ответ верен =  %r" % answer.text)
 else:
     print("Ожидаемый ответ ", answer.text, " не найден")
 
-time.sleep(3)
+time.sleep(2)
 # driver.quit()
 
 
 # TК-006	Проверка функции сортировки товара
 sort_button = driver.find_element(By.XPATH,
-                                  '//*[@id="plp"]/div/div[2]/div/div/div/div[1]/ul/li[3]/div/button')
+                                  '//*[@id="plp"]/div/div[2]/div/div/div/div[1]/ul/li[4]/div/button')
 time.sleep(1)
 sort_button.click()
 time.sleep(1)
-man_sort = driver.find_element(By.XPATH,
-                               '//*[@id="plp"]/div/div[2]/div/div/div/div[1]/ul/li[3]/div/div/div/ul/li[1]/div/label/div')
-man_sort.click()
+type_sort = driver.find_element(By.XPATH,
+                                '//*[@id="plp"]/div/div[2]/div/div/div/div[1]/ul/li[4]/div/div/div/ul/li[1]/div/label/div')
+type_sort.click()
 time.sleep(1)
-print('Сортировка по полу успешна')
+sort_button.click()
+print('№2 Сортировка успешна')
 
 
 # TК-004	Проверка функции добавление товара в Избранное
 
 sell_box = driver.find_element(By.XPATH,
-                               '//*[@id="product-20774938"]/a')
+                               '//*[@id="product-201308084"]/a')
 
 sell_box.click()
 time.sleep(1)
@@ -55,8 +58,38 @@ wish_list_button = driver.find_element(By.XPATH,
                                        '//*[@id="chrome-sticky-header"]/div[1]/div/ul/li[2]/a')
 wish_list_button.click()
 
-print('Товар добавлен в список желаемого')
+print('№3 Товар добавлен в список желаемого')
 time.sleep(1)
 
 
+# TК-007	Проверка функции добавления товара в корзину
+sell_box2 = driver.find_element(By.XPATH,
+                                '//*[@id="saved-lists-app"]/main/div/div[1]/section/ol/li/div/div/div/a')
+sell_box2.click()
+time.sleep(0.5)
 
+add_bag_button = driver.find_element(By.XPATH,
+                                     '//*[@id="product-add-button"]')
+add_bag_button.click()
+time.sleep(0.5)
+
+bag_button = driver.find_element(By.XPATH,
+                                 '//*[@id="chrome-sticky-header"]/div[1]/div/ul/li[3]/a')
+bag_button.click()
+time.sleep(3)
+print('№4 Товар добавлен в корзину')
+
+# ТК-002 Удаление товара из корзины
+del_button = driver.find_element(By.XPATH,
+                                 '//*[@id="bagApp"]/div[1]/div/div[1]/div[1]/bag-group-list/ul/li/bag-item-list/ul/li/div/div/div/bag-remove/div/button')
+
+del_button.click()
+time.sleep(5)
+
+
+# TК-003	Проверка функции перехода по ссылке на социальные сети сайта через кнопку
+
+insta_button = driver.find_element(By.XPATH,
+                                   '//*[@id="chrome-footer"]/footer/div[1]/div[1]/ul[1]/li[2]/a')
+insta_button.click()
+print('№5 Переход по кнопке успешен')
